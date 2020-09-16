b900ef0cb732b4a421258174c38f63bb0a52a818
Rania Zarka <rani0401@edu.easj.dk>
2020-09-16 18:47:29 +0200

    task 3

--- ClassLibrary/MC.cs
+++ ClassLibrary/MC.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ClassLibrary
 {
-    public class Car
-    { /// <summary>
-    /// instance fields string _liscencePlate, DateTime _date
-    /// </summary>
+   public  class MC
+    {
+        /// <summary>
+        /// instance fields string _liscencePlate, DateTime _date
+        /// </summary>
         private string _liscenseplate;
         private DateTime _date;
 
-        public Car()
+        public MC()
         {
         }
 
@@ -18,7 +21,7 @@ namespace ClassLibrary
         /// </summary>
         /// <param name="LiscencePlate"></param>
         /// <param name="Date"></param>
-        public Car(string LiscencePlate, DateTime Date)
+        public MC(string LiscencePlate, DateTime Date)
         {
             _liscenseplate = LiscencePlate;
             _date = Date;
@@ -30,21 +33,22 @@ namespace ClassLibrary
         public string LiscencePlate { get; set; }
         public string Date { get; set; }
 
+
         /// <summary>
-        /// public double method Price that returns 240
+        /// public double method Price that returns 125
         /// </summary>
         /// <returns></returns>
         public double Price()
         {
-            return 240;
+            return 125;
         }
         /// <summary>
-        /// public string method VehiculeType that returns "Car" 
+        /// public string method Vehicule that returns "MC" 
         /// </summary>
         /// <returns></returns>
-        public string VehiculeType()
+        public string Vehicule()
         {
-            return "Car";
+            return "MC";
         }
     }
 }
--- TicketingTests/UnitTest1.cs
+++ TicketingTests/UnitTest1.cs
@@ -7,20 +7,38 @@ namespace TicketingTests
     public class UnitTest1
     {
 
-        car c = new car();
+        Car c = new Car();
+        MC mc = new MC();
 
         [TestMethod]
-        public void TestPriceMethod()
+        public void TestPriceCarMethod()
         {
             double thePrice = c.Price();
             Assert.AreEqual(240, thePrice);
         }
 
         [TestMethod]
-        public void TestVehiculeTypeMethod()
+        public void TestVehiculeTypeCarMethod()
         {
             string theType = c.VehiculeType();
             Assert.AreEqual("Car", theType);
         }
+
+        [TestMethod]
+        public void TestPriceMCMethod()
+        {
+            double thePrice = mc.Price();
+            Assert.AreEqual(125, thePrice);
+        }
+
+        [TestMethod]
+        public void TestVehiculeTypeMCMethod()
+        {
+            string theType = mc.Vehicule();
+            Assert.AreEqual("MC", theType);
+        }
+
+
+
     }
 }
